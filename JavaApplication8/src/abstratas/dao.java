ee73cfe7d910c0551823c496f2e4c7ad886e7730
JadsonLOliveira <jadson.oliveira@alunos.sc.senac.br>
2022-06-21 20:18:51 -0300

    Cadastrando DAO

---
+++ JavaApplication8/src/abstratas/dao.java
@@ -0,0 +1,32 @@
+
+package abstratas;
+
+import java.util.List;
+import javax.persistence.Query;
+
+abstract public class dao<T> {
+    public void create(T object){
+        Dados.getManager().getTransaction().begin();
+        Dados.getManager().persist(object);
+        Dados.getManager().getTransaction().commit();
+    }
+    public void update(T object){
+        Dados.getManager().getTransaction().begin();
+        Dados.getManager().merge(object);
+        Dados.getManager().getTransaction().commit();
+    }
+    public void delete(T object){
+        Dados.getManager().getTransaction().begin();
+        Dados.getManager().remove(object);
+        Dados.getManager().getTransaction().commit();
+    }
+    public T read(Class<T> classtoReturn, int id){
+        return (Dados.getManager().find(classtoReturn, id));
+    }
+    //
+    public List<T> read(String jpql){
+        Query query = Dados.getManager().createQuery(jpql);
+        return query.getResultList();
+    }
+    
+}
