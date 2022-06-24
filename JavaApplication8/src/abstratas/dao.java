edba0cba6eb2b0376ac444afede8d1b0e790bf6b
JadsonLOliveira <jadson.oliveira@alunos.sc.senac.br>
2022-06-24 19:23:59 -0300

    Projeto inicial

--- JavaApplication8/src/abstratas/dao.java
+++ JavaApplication8/src/abstratas/dao.java
@@ -29,4 +29,13 @@ abstract public class dao<T> {
         return query.getResultList();
     }
     
+    public List<T> read(String JPQL,Object... args){        
+        Query query = Dados.getManager().createQuery(JPQL);
+        for(int i=0;i<args.length;i++){
+            query.setParameter((i+1), args[i]);
+        }
+        return query.getResultList();                
+    }
+    
+    
 }
